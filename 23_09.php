e1d53f50531b7772bd35205569a0794e506e57eb
student <bartek.musial@hotmail.com>
2019-09-23 09:34:56 +0200

    zmiany

--- 23_09.php
+++ 23_09.php
@@ -31,4 +31,51 @@ echo $x1;
 $text2=100;
 $x2=(unset)$text2;
 echo $x2;
+
+#########################################
+
+$c;
+echo $c;
+echo @gettype($c);//NULL
+
+echo PHP_INT_SIZE;//4
+
+//echo phpinfo();//PHP Version 7.2.1
+
+########################################
+
+$x=10;
+echo is_string($x);//false
+echo is_int($x);//1=
+echo is_bools($x);
+echo is_float($x);
+echo is_null($x);
+
+##################################
+
+//zmienne superglobalne
+
+//$_GET, $_POST, $_COOKIE, $_FILES, $_SESSION, $_SEREVER
+
+echo $_SERVER['SERVER_PORT'];
+echo $_SEREVER['SERVER_NAME'];
+echo $SERVER['SCRIPT_NAME'];
+echo $_SERVER['SERVER_PROTOCOL'];
+echo $_SERVER['DOCUMENT_ROOT'];
+
+$lokalPliku = $_SERVER['DOCUMENT_ROOT'];
+$_SERVER['SCRIPT_NAME'];
+
+echo $lokalPliku,'<hr>';
+
+###########################################
+
+define('NAZWISKO','KOWAL');
+echo NAZWISKO;
+
+define('imie','Janusz');
+echo imie;
+
+define ('WIEK',18,true);
+echo wiek;
 ?>
